e136cdac4002b99e6af4db8431c4b8039fc2e756
boongbaboo15 <boongababoo15@gmail.com>
2023-02-15 14:23:15 -0800

    Unit Test NameNull - close #2

--- backend/entity/b6323059_positive_test.go
+++ backend/entity/b6323059_positive_test.go
@@ -7,7 +7,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func TestEmployeePass(t *testing.T) {
+func TestEmployeePass(t *testing.T) { //Pass
 	g := NewGomegaWithT(t)
 
 	employee := Employee{
@@ -20,4 +20,20 @@ func TestEmployeePass(t *testing.T) {
 
 	g.Expect(ok).To(BeTrue())
 	g.Expect(err).To(BeNil())
+}
+
+func TestNameNull(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	employee := Employee{
+		Name:       "",
+		Email:      "test@gmail.com",
+		EmployeeID: "J12345678",
+	}
+
+	ok, err := govalidator.ValidateStruct(employee)
+
+	g.Expect(ok).ToNot(BeTrue())
+	g.Expect(err).ToNot(BeNil())
+	g.Expect(err.Error()).To(Equal("Name cannot be blank"))
 }
